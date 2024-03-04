5e9991aa123f8a7c0ec0325bd6bf294e934b890a
Tod Kurt <todbot@users.noreply.github.com>
2024-03-04 09:54:42 -0800

    bandage fix rtd compile

--- synth_tools/__init__.py
+++ synth_tools/__init__.py
@@ -14,21 +14,13 @@ CircuitPython helper library to do help doing synthio
 Implementation Notes
 --------------------
 
-**Hardware:**
-
-.. todo:: Add links to any specific hardware product page(s), or category page(s).
-  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
-
 **Software and Dependencies:**
 
 * Adafruit CircuitPython firmware for the supported boards:
   https://circuitpython.org/downloads
 
-.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
-  based on the library's use of either.
+* synthio : https://docs.circuitpython.org/en/latest/shared-bindings/synthio/
 
-# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
-# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
 """
 
 # imports
