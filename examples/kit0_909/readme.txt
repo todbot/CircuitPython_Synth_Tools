3a78b32372c03123f227e52a64388ec6b6355964
Tod Kurt <todbot@users.noreply.github.com>
2025-05-11 10:31:18 -0700

    trig sequencer demo

---
+++ examples/kit0_909/readme.txt
@@ -0,0 +1,4 @@
+
+Some sounds from:
+- 909 drums - https://freesound.org/people/GRD-music-/packs/23395/
+- Amen break
--- examples/trig_sequencer_demo.py
+++ examples/trig_sequencer_demo.py
@@ -1,41 +1,55 @@
+#
+
+# adding this can help with timing, if you need it
+# import microcontroller
+# microcontroller.cpu.frequency = 200_000_000
+
 import time
 import audiocore, audiomixer
-from synth_setup import audio, SAMPLE_RATE, BUFFER_SIZE, knobA, knobB, keys
-from trig_sequencer import TrigSequencer, ticks_ms
+from synth_setup import audio, SAMPLE_RATE, BUFFER_SIZE
+from trig_sequencer import TrigSequencer
 
 bpm = 120
-trig_count = 3
+trig_count = 4
 step_count = 16
-steps_per_beat = 4 # 2 = 8th note, 4 = 16th note
-
-mixer = audiomixer.Mixer(voice_count=trig_count, channel_count=1,
-                         sample_rate=SAMPLE_RATE, buffer_size=BUFFER_SIZE)
-audio.play(mixer)
-
-def trig_on(trig_num, wav):
-    global last_ms
-    mixer.voice[trig_num].play(wav, loop=False)
-    print("trig_on:    %d" % (trig_num)) #midi_note, vel, gate, on), ticks_ms())
-    
-def trig_off(trig_num, wav): # midi_note, vel, gate, on):
-    print("  trig_off: %3d" % (trig_num,)) #midi_note, vel, gate, on), int(ticks_ms()-last_ms))
-
+steps_per_beat = 4  # 2 = 8th note, 4 = 16th note
 
 drum_pattern1 = (
-    (1,0,0,0, 1,0,0,0, 1,0,0,0, 1,0,0,1),   # bass drum
-    (0,0,1,0, 0,0,1,0, 0,0,1,0, 0,0,1,0),   # snare
-    (1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1),   # hi hat
+    (1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1),  # bass drum
+    (0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0),  # snare
+    (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0),  # hi hat
+    (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),  # hi hat open
 )
 
-#drum_map = (36, 38, 42, 0, 0, 0, 0, 0)  # general midi drums
-drum_map = (36, 48, 72, 0, 0, 0, 0, 0)  # midi notes 
-drum_map = (
+drum_map = (36, 48, 72, 0, 0, 0, 0, 0)  # could be midi notes
+drum_map = (  # or anything else your callback wants
     audiocore.WaveFile("/kit0_909/00_909kick4.wav"),
     audiocore.WaveFile("/kit0_909/01_909snare2.wav"),
     audiocore.WaveFile("/kit0_909/02_909hatclosed2a.wav"),
+    audiocore.WaveFile("/kit0_909/03_909hatopen5.wav"),
 )
 
-seq = TrigSequencer(trig_count, step_count, steps_per_beat, on_func=trig_on, off_func=trig_off)
+
+def trig_on(trig_num, wav):
+    mixer.voice[trig_num].play(wav, loop=False)
+    print("trig_on:    %d" % (trig_num))
+
+
+def trig_off(trig_num, wav):
+    print("  trig_off: %3d" % trig_num, wav)
+
+
+mixer = audiomixer.Mixer(
+    voice_count=trig_count,
+    channel_count=1,
+    sample_rate=SAMPLE_RATE,
+    buffer_size=BUFFER_SIZE,
+)
+audio.play(mixer)
+
+seq = TrigSequencer(
+    trig_count, step_count, steps_per_beat, on_func=trig_on, off_func=trig_off
+)
 seq.bpm = bpm
 seq.set_pattern(drum_pattern1)
 seq.set_drum_map(drum_map)
@@ -46,6 +60,4 @@ print("pattern:", seq.trigs)
 
 while True:
     seq.update()
-    #time.sleep(0.001)
-
-
+    time.sleep(0.0001)
