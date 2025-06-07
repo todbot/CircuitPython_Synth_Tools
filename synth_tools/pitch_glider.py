8dc327b60014589249d6194a1419b396ee8d5222
Tod Kurt <todbot@users.noreply.github.com>
2025-06-07 13:39:13 -0700

    updated pitch_glider from tbish

--- synth_tools/pitch_glider.py
+++ synth_tools/pitch_glider.py
@@ -6,23 +6,22 @@
 
 `Glider` is a portamento feature for synthio.Notes. Attach to note.bend.
 
-Part of synth_tools.
+10 Feb 2025 - @todbot / Tod Kurt
 
 """
+# pitch_glider.py --
+# part of todbot circuitpython synthio tutorial
+# 10 Feb 2025 - @todbot / Tod Kurt
 
 import synthio
 import ulab.numpy as np
-
-
-def bend_amount(old_midi_note, new_midi_note):
-    """Calculate how much note.bend has to happen between two notes"""
-    return (new_midi_note - old_midi_note) * (1 / 12)
 
 
 class Glider:
     """Attach a Glider to note.bend to implement portamento"""
 
     def __init__(self, glide_time, midi_note):
+        glide_time = glide_time or 0.001
         self.pos = synthio.LFO(
             once=True,
             rate=1 / glide_time,
@@ -33,12 +32,16 @@ class Glider:
 
     def update(self, new_midi_note):
         """Update the glide destination based on new midi note"""
-        self.lerp.a = bend_amount(new_midi_note, self.midi_note)
+        self.lerp.a = self.bend_amount(new_midi_note, self.midi_note)
         self.lerp.b = 0  # end on the new note
         self.pos.retrigger()  # restart the lerp
         # print("bend_amount:", self.bend_amount(self.midi_note, new_midi_note),
         #      "old", self.midi_note, "new:", new_midi_note, self.lerp.a, self.lerp.b)
         self.midi_note = new_midi_note
+
+    def bend_amount(self, old_midi_note, new_midi_note):
+        """Calculate how much note.bend has to happen between two notes"""
+        return (new_midi_note - old_midi_note) * (1 / 12)
 
     @property
     def glide_time(self):
@@ -48,4 +51,5 @@ class Glider:
     @glide_time.setter
     def glide_time(self, glide_time):
         """Set glide time in seconds, sets the rate of underlying LFO"""
+        glide_time = glide_time or 0.001  # ensure non-zero for division
         self.pos.rate = 1 / glide_time
